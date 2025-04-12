54b65332218ea3fedb1e3017aeda52a6205cb936
mcpeleeGit <mcpelee@gmail.com>
2025-04-12 20:48:58 +0900

    update config

--- app/Repositories/Common/Repository.php
+++ app/Repositories/Common/Repository.php
@@ -11,10 +11,10 @@ class Repository {
         global $config; // 전역 변수로 설정을 가져옵니다.
 
         // 서버 호스트 이름을 확인하여 데이터베이스 설정 선택
-        $databaseConfig = ($_SERVER['HTTP_HOST'] === 'localhost') ? $config['sandbox_database'] : $config['real_database'];
+        $databaseConfig = ($_SERVER['HTTP_HOST'] === 'localhost:8000') ? $config['sandbox_database'] : $config['real_database'];
 
         $dsn = 'mysql:host=' . $databaseConfig['host'] . ';port=' . $databaseConfig['port'] . ';dbname=' . $databaseConfig['dbname'] . ';charset=utf8mb4';
-        $this->pdo = new PDO($dsn, $config['database']['username'], $config['database']['password']);
+        $this->pdo = new PDO($dsn, $databaseConfig['username'], $databaseConfig['password']);
         $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
